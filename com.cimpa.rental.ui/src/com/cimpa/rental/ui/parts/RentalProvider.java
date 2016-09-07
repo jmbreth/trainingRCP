73537022b60f246236e1192017ea5721bb4621e6
Zenika <Zenika@ZNK_FORMATION>
2016-09-07 16:01:14 +0200

    Couleurs

--- com.cimpa.rental.ui/src/com/cimpa/rental/ui/parts/RentalProvider.java
+++ com.cimpa.rental.ui/src/com/cimpa/rental/ui/parts/RentalProvider.java
@@ -2,14 +2,18 @@ package com.cimpa.rental.ui.parts;
 
 import java.util.Collection;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class RentalProvider extends LabelProvider implements ITreeContentProvider {
+public class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
 
 	@Override
 	public Object[] getElements(Object inputElement) {
@@ -89,6 +93,21 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 				return a.getObjectsToRent().toArray();
 			return null;
 		}
+	}
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_CYAN);
+		}
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
